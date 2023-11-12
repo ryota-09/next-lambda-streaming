a4d8f1d024326289db4d9f1695fb8ace506804e8
ryota-09 <n.ryo950911@gmail.com>
2023-11-12 14:16:04 +0900

    sam用のファイル作成

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: "standalone",
+  compress: true
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

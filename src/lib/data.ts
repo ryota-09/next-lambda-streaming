9893bfdcc3500e41c20c71f33a8a119f13f011a3
ryota-09 <n.ryo950911@gmail.com>
2023-11-12 16:00:48 +0900

    cacheの変更

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -3,11 +3,11 @@ export async function fetchPosts() {
     await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
-      cache: "no-cache",
+      cache: "no-store",
     });
 
-    const data = await response.json()
-    return data
+    const data = await response.json();
+    return data;
   } catch (error) {
     throw new Error("Failed to fetch posts data.");
   }
@@ -18,11 +18,11 @@ export async function fetchTodos() {
     await new Promise((resolve) => setTimeout(resolve, 6000));
 
     const response = await fetch("https://jsonplaceholder.typicode.com/todos", {
-      cache: "no-cache",
+      cache: "no-store",
     });
 
-    const data = await response.json()
-    return data
+    const data = await response.json();
+    return data;
   } catch (error) {
     throw new Error("Failed to fetch todos data.");
   }

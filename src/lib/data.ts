88dfdac5cfda91a84dbed8048f512fd3e7a723f1
ryota-09 <n.ryo950911@gmail.com>
2023-11-12 20:37:52 +0900

    trycatch削除

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,44 +1,31 @@
 export async function fetchPosts() {
-  try {
-    await new Promise((resolve) => setTimeout(resolve, 3000));
+  await new Promise((resolve) => setTimeout(resolve, 3000));
 
-    const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
-      cache: "no-store",
-    });
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
+    cache: "no-store",
+  });
 
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    throw new Error("Failed to fetch posts data.");
-  }
+  const data = await response.json();
+  return data;
 }
 
 export async function fetchTodos() {
-  try {
-    // await new Promise((resolve) => setTimeout(resolve, 6000));
+  // await new Promise((resolve) => setTimeout(resolve, 6000));
 
-    const response = await fetch("https://jsonplaceholder.typicode.com/todos", {
-      cache: "no-store",
-    });
+  const response = await fetch("https://jsonplaceholder.typicode.com/todos", {
+    cache: "no-store",
+  });
 
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    throw new Error("Failed to fetch todos data.");
-  }
+  const data = await response.json();
+  return data;
 }
 
 export async function fetchText() {
-  try {
+  const response = await fetch("https://hub.dummyapis.com/delay?seconds=8", {
+    cache: "no-store",
+  });
 
-    const response = await fetch("https://hub.dummyapis.com/delay?seconds=8", {
-      cache: "no-store",
-    });
+  console.log(response);
 
-    console.log(response)
-
-    return "サクセス"
-  } catch (error) {
-    throw new Error("Failed to fetch todos data.");
-  }
+  return "サクセス";
 }

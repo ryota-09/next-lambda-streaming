3f8e6172777dc4b38a07aa82fa47fe5535660fe2
ryota-09 <n.ryo950911@gmail.com>
2023-11-12 13:03:59 +0900

    static Area作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,28 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <h1 className={`mb-4 text-xl md:text-2xl`}>Next.js App on Lambda Web Adapter</h1>
+      <h1 className={`mb-4 text-xl md:text-2xl`}>
+        Next.js App on Lambda Web Adapter
+      </h1>
+      <div className="flex justify-center gap-5">
+        <Image alt="" src="/red.png" width={300} height={200} />
+        <div>
+          <h2 className="text-xl font-bold">Static Area</h2>
+          <br />
+          <p>
+            テキストテキストテキストテキストテキストテキストテキストテキスト
+          </p>
+          <p>
+            テキストテキストテキストテキストテキストテキストテキストテキスト
+          </p>
+          <p>
+            テキストテキストテキストテキストテキストテキストテキストテキスト
+          </p>
+          <p>
+            テキストテキストテキストテキストテキストテキストテキストテキスト
+          </p>
+        </div>
+      </div>
     </main>
   );
 }
